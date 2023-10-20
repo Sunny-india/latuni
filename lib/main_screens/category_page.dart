08eaeca3734b237d9647f1ff3741dded35f9278c
sunnychawla <shrichawlaplastics14@gmail.com>
2023-10-21 01:37:38 +0530

    in-between video 13

--- lib/main_screens/category_page.dart
+++ lib/main_screens/category_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:latuni/my_widgets/fake_search.dart';
 
@@ -7,7 +9,7 @@ List<CategoryData> categories = [
   CategoryData(label: 'Kisan Bhai'),
   CategoryData(label: 'Dairy'),
   CategoryData(label: 'Kirana Grocery'),
-  CategoryData(label: 'Grocery'),
+  CategoryData(label: 'Clothing'),
 ];
 
 class CategoryPage extends StatefulWidget {
@@ -18,6 +20,20 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+  @override
+  void initState() {
+    // whenever uses lands on this page,
+    // to show him/her the by default page
+    for (var item in categories) {
+      item.isSelected = false;
+    }
+    categories[0].isSelected = true;
+
+    super.initState();
+  }
+
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
@@ -55,15 +71,29 @@ class _CategoryPageState extends State<CategoryPage> {
           itemBuilder: (context, index) {
             return InkWell(
               onTap: () {
+                /// the whole this code is functioned
+                /// through the pageView changed call back down
                 setState(() {
-                  for (var item in categories) {
-                    item.isSelected = false;
-                  }
-                  categories[index].isSelected = true;
+                  // for (var item in categories) {
+                  //   item.isSelected = false;
+                  // }
+                  // categories[index].isSelected = true;
+                  /// and this code is for pageview actually
+                  _pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 900),
+                    curve: Curves.easeInCirc,
+                  );
                 });
               },
               child: Container(
-                height: size.height / categories.length,
+                height: categories[index].isSelected
+                    ? 80
+                    : size.height / categories.length,
+                width: size.width * .23,
+                margin: categories[index].isSelected
+                    ? EdgeInsets.symmetric(horizontal: 5, vertical: 5)
+                    : const EdgeInsets.only(left: 5, bottom: 2),
                 decoration: BoxDecoration(
                   color: categories[index].isSelected
                       ? Colors.grey.shade100
@@ -72,25 +102,50 @@ class _CategoryPageState extends State<CategoryPage> {
                       ? null
                       : LinearGradient(
                           colors: [
-                              Colors.blue.shade100,
-                              Colors.blue.shade300,
-                              Colors.blue.shade500,
-                              Colors.blue.shade600
+                              Colors.red.shade100,
+                              Colors.red.shade50,
+                              Colors.red.shade100,
+                              Colors.red.shade50,
+                              Colors.redAccent
                             ],
                           stops: const [
                               0.2,
                               0.4,
                               .6,
-                              .8
+                              .8,
+                              .9
                             ],
                           begin: Alignment.topLeft,
                           end: Alignment.bottomRight),
+                  borderRadius: categories[index].isSelected
+                      ? BorderRadius.circular(12)
+                      : null,
+                  boxShadow: categories[index].isSelected
+                      ? [
+                          BoxShadow(
+                              color: Colors.grey.shade200,
+                              offset: const Offset(-2, -2),
+                              spreadRadius: 2,
+                              blurRadius: 2),
+                          BoxShadow(
+                              color: Colors.grey.shade300,
+                              offset: const Offset(
+                                2,
+                                2,
+                              ),
+                              spreadRadius: 2,
+                              blurRadius: 2)
+                        ]
+                      : null,
                 ),
-                child: Center(
-                  child: Text(
-                    categories[index].label.toString(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.black, fontSize: 18),
+                child: Transform.rotate(
+                  angle: pi / -3,
+                  child: Center(
+                    child: Text(
+                      categories[index].label.toString(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.black, fontSize: 18),
+                    ),
                   ),
                 ),
               ),
@@ -106,7 +161,33 @@ class _CategoryPageState extends State<CategoryPage> {
       decoration: BoxDecoration(
         color: Colors.grey.shade100,
       ),
-      child: const Center(child: Text('Right Container')),
+      child: PageView(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        onPageChanged: (value) {
+          /// the whole code is originally from listView.builder from above
+          setState(() {
+            for (var item in categories) {
+              item.isSelected = false;
+            }
+            categories[value].isSelected = true;
+          });
+        },
+        children: [
+          Center(
+            child: Text('Page One for Kisan Bhai category'),
+          ),
+          Center(
+            child: Text('Page two for Dairy category'),
+          ),
+          Center(
+            child: Text('Page three for Kirana category'),
+          ),
+          Center(
+            child: Text('Page four for Clothing category'),
+          ),
+        ],
+      ),
     );
   }
 }
