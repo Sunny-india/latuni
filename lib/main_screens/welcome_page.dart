f9313e2f9d581f6de9f08534a4c39822d35f7ae3
itc <shrichawlaplastics14@gmail.com>
2023-11-05 10:14:11 +0530

    Welcome Screen being better

--- lib/main_screens/welcome_page.dart
+++ lib/main_screens/welcome_page.dart
@@ -13,21 +13,32 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   final Color backgroundColor = Colors.grey.shade300;
+  late List<Map<String, dynamic>> socialMediaList;
+  @override
+  void initState() {
+    socialMediaList = [
+      {
+        'image': 'assets/images/logos/fb.png',
+        'label': 'FaceBook',
+        'onPressed': faceBookLogin
+      },
+      {
+        'image': 'assets/images/logos/google.png',
+        'label': 'Google',
+        'onPressed': googleLogin
+      },
+    ];
 
-  // bool doItClicked = false;
-  // bool signUpClicked = false;
-  // void doItTapped() {
-  //   setState(() {
-  //     doItClicked = !doItClicked;
-  //     signUpClicked = false;
-  //   });
-  // }
-  //
-  // void signUpTapped() {
-  //   setState(() {
-  //     signUpClicked = !signUpClicked;
-  //   });
-  // }
+    super.initState();
+  }
+
+  void faceBookLogin() {
+    print('FaceBook');
+  }
+
+  void googleLogin() {
+    print('Google');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,108 +46,125 @@ class _WelcomePageState extends State<WelcomePage> {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(
-            left: size.width * .04,
-            right: size.width * .04,
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              //crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                SizedBox(
-                  height: size.height * .08,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 20,
+                  left: size.width * .04,
+                  right: size.width * .04,
                 ),
+                child: Column(
+                  children: [
+                    /// WELCOME text
+                    ClipPath(
+                      clipper: ParchiBottom(),
+                      child: welcomeTextContainer(size),
+                    ),
+                    const SizedBox(height: 20),
 
-                /// WELCOME text
-                ClipPath(
-                  clipper: ParchiBottom(),
-                  child: welcomeTextContainer(size),
-                ),
-                const SizedBox(height: 20),
-
-                /// Suppliers login/signup
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 6),
-                  height: size.height * .1,
-                  width: size.width * .9,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(21),
-                      color: Colors.orange),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Text('S U P P L I E R S'),
-                      const SizedBox(width: 10),
-                      MyButton(
-                          mWidth: size.width * .2,
-                          title: const Text('LOGIN'),
-                          onTapped: () {}),
-                      const SizedBox(width: 10),
-                      MyButton(
-                          mWidth: size.width * .2,
-                          title: const Text('SIGN UP'),
-                          onTapped: () {}),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-
-                /// customers login/signup
-
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 6),
-                  height: size.height * .1,
-                  width: size.width * .9,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(21),
-                      color: Colors.orange),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const RotatedBox(
-                          quarterTurns: 2, child: Text('C U S T O M E R S')),
-                      const SizedBox(width: 10),
-                      MyButton(
-                          mWidth: size.width * .2,
-                          title: const Text('LOGIN'),
-                          onTapped: () {}),
-                      const SizedBox(width: 10),
-                      MyButton(
-                          mWidth: size.width * .2,
-                          title: const Text('SIGN UP'),
-                          onTapped: () {}),
-                    ],
-                  ),
-                ),
-
-                /// alternate design
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: size.height * .35,
-                  width: size.width * .9,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: ClipPath(
-                          clipper: RightClipper(),
-                          child: const LeftSupplierContainer(),
-                        ),
+                    /// Suppliers login/signup
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 6),
+                      height: size.height * .1,
+                      width: size.width * .9,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(21),
+                          color: Colors.orange),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Text('S U P P L I E R S'),
+                          const SizedBox(width: 10),
+                          MyButton(
+                              mWidth: size.width * .2,
+                              title: const Text('LOGIN'),
+                              onTapped: () {}),
+                          const SizedBox(width: 10),
+                          MyButton(
+                              mWidth: size.width * .2,
+                              title: const Text('SIGN UP'),
+                              onTapped: () {}),
+                        ],
                       ),
-                      Expanded(
-                        child: ClipPath(
-                          clipper: LeftClipper(),
-                          child: const RightCustomerContainer(),
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    /// customers login/signup
+
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 6),
+                      height: size.height * .1,
+                      width: size.width * .9,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(21),
+                          color: Colors.orange),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const RotatedBox(
+                              quarterTurns: 2,
+                              child: Text('C U S T O M E R S')),
+                          const SizedBox(width: 10),
+                          MyButton(
+                              mWidth: size.width * .2,
+                              title: const Text('LOGIN'),
+                              onTapped: () {}),
+                          const SizedBox(width: 10),
+                          MyButton(
+                              mWidth: size.width * .2,
+                              title: const Text('SIGN UP'),
+                              onTapped: () {}),
+                        ],
+                      ),
+                    ),
+
+                    /// alternate design
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      height: size.height * .35,
+                      width: size.width * .9,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: ClipPath(
+                              clipper: RightClipper(),
+                              child: const LeftSupplierContainer(),
+                            ),
+                          ),
+                          Expanded(
+                            child: ClipPath(
+                              clipper: LeftClipper(),
+                              child: const RightCustomerContainer(),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 80),
+                  ],
                 ),
-                const SizedBox(height: 80),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: size.height * .04,
+              ),
+
+              /// bottom Row for social sign ins
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: List.generate(socialMediaList.length, (index) {
+                      return BottomSocialWidgets(
+                          onPressed: socialMediaList[index]['onPressed'],
+                          image: socialMediaList[index]['image'],
+                          label: socialMediaList[index]['label']);
+                    })),
+              ),
+            ],
           ),
         ),
         // bottom: true,
@@ -171,40 +199,43 @@ class RightCustomerContainer extends StatelessWidget {
     Size size = MediaQuery.sizeOf(context);
     return Container(
       decoration: const BoxDecoration(
-        color: Colors.tealAccent,
+        color: Colors.deepPurple,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const SizedBox(height: 10),
-          const RotatedBox(
-            quarterTurns: 3,
-            child: Text(
-              'C U S T O M E R S',
+      child: FittedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(height: 10),
+            const RotatedBox(
+              quarterTurns: 3,
+              child: Text(
+                'C U S T O M E R S',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          MyButton(
-              mWidth: size.width * .35,
-              title: const Text('Login'),
-              onTapped: () {
-                //todo: login later
-              }),
-          const SizedBox(
-            height: 10,
-          ),
-          MyButton(
-              mWidth: size.width * .35,
-              title: const Text('Sign Up'),
-              onTapped: () {
-                //todo: Sign Up later
-              }),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            MyButton(
+                mWidth: size.width * .35,
+                title: const Text('Login'),
+                onTapped: () {
+                  //todo: login later
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+            MyButton(
+                mWidth: size.width * .35,
+                title: const Text('Sign Up'),
+                onTapped: () {
+                  //todo: Sign Up later
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -220,40 +251,76 @@ class LeftSupplierContainer extends StatelessWidget {
     Size size = MediaQuery.sizeOf(context);
     return Container(
       decoration: const BoxDecoration(
-        color: Colors.red,
+        color: Colors.deepPurple,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const SizedBox(height: 12),
-          const RotatedBox(
-            quarterTurns: 3,
-            child: Text(
-              'S U P P L I E R S',
+      child: FittedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(height: 12),
+            const RotatedBox(
+              quarterTurns: 3,
+              child: Text(
+                'S U P P L I E R S',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            MyButton(
+                mWidth: size.width * .35,
+                title: const Text('Login'),
+                onTapped: () {
+                  //todo: login later
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+            MyButton(
+                mWidth: size.width * .35,
+                title: const Text('Sign Up'),
+                onTapped: () {
+                  //todo: Sign Up later
+                }),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BottomSocialWidgets extends StatelessWidget {
+  const BottomSocialWidgets(
+      {super.key,
+      required this.image,
+      required this.label,
+      required this.onPressed});
+  final String image;
+  final String label;
+  final Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: SizedBox(
+        width: 70,
+        height: 60,
+        //  decoration: const BoxDecoration(color: Colors.deepPurple),
+        child: FittedBox(
+          child: Column(
+            children: [
+              Image.asset(
+                image,
+                height: 50,
+              ),
+              Text(label),
+            ],
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          MyButton(
-              mWidth: size.width * .35,
-              title: const Text('Login'),
-              onTapped: () {
-                //todo: login later
-              }),
-          const SizedBox(
-            height: 10,
-          ),
-          MyButton(
-              mWidth: size.width * .35,
-              title: const Text('Sign Up'),
-              onTapped: () {
-                //todo: Sign Up later
-              }),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
+        ),
       ),
     );
   }
